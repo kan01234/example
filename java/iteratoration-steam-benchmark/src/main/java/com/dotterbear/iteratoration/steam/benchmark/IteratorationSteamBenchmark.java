aed58968025bfc10d92b3d1d25e8573ab75ec025
Kan <safghjkl@gmail.com>
2020-03-27 14:52:05 +0900

    Create IteratorationSteamBenchmark.java

---
+++ java/iteratoration-steam-benchmark/src/main/java/com/dotterbear/iteratoration/steam/benchmark/IteratorationSteamBenchmark.java
@@ -0,0 +1,49 @@
+package com.dotterbear.iteratoration.steam.benchmark;
+
+import org.openjdk.jmh.annotations.*;
+
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@BenchmarkMode(Mode.AverageTime)
+@OperationsPerInvocation(IteratorationSteamBenchmark.N)
+public class IteratorationSteamBenchmark {
+
+  public static final int N = 10000;
+
+    static List<Integer> numsArrayList = new ArrayList<>();
+    static {
+        for (int i = 0; i < N; i++) {
+            numsArrayList.add(i);
+        }
+    }
+
+    @Benchmark
+    public List<Double> for1() {
+      List<Integer> results = new ArrayList<>(N / 2 + 1);
+      for (int i = 0; i < N; i++) {
+        if (i % 2 == 0)
+          results.add(Math.sqrt(i))
+      }
+      return results;
+    }
+
+    @Benchmark
+    public List<Double> for2() {
+      List<Integer> results = new ArrayList<>(numsArrayList.size()) / 2 + 1);
+      for (int i = 0; i < numsArrayList.size(); i++) {
+        if (i % 2 == 0)
+          results.add(Math.sqrt(i))
+      }
+      return results;
+    }
+
+    @Benchmark
+    public List<Double> for3() {
+      List<Integer> results = new ArrayList<>(numsArrayList.size()) / 2 + 1);
+      for (int i : numsArrayList.size()) {
+        if (i % 2 == 0)
+          results.add(Math.sqrt(i))
+      }
+      return results;
+    }
+
+}

0fdfb98bab2595da7046abaabb32db6f5e1ffd74
Kan <safghjkl@gmail.com>
2020-03-27 16:17:57 +0900

    Update App.java

--- java/iteratoration-steam-benchmark/src/main/java/com/dotterbear/iteratoration/steam/benchmark/App.java
+++ java/iteratoration-steam-benchmark/src/main/java/com/dotterbear/iteratoration/steam/benchmark/App.java
@@ -1,19 +1,19 @@
-package com.dotterbear.iteratoration.steam.benchmark;
+// package com.dotterbear.iteratoration.steam.benchmark;
 
-import org.openjdk.jmh.annotations.*;
-import org.openjdk.jmh.runner.Runner;
-import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
+// import org.openjdk.jmh.annotations.*;
+// import org.openjdk.jmh.runner.Runner;
+// import org.openjdk.jmh.runner.RunnerException;
+// import org.openjdk.jmh.runner.options.Options;
+// import org.openjdk.jmh.runner.options.OptionsBuilder;
 
-public class App {
+// public class App {
 
-  public static void main(String[] args) throws RunnerException {
-    Options opt = new OptionsBuilder()
-                    .include(IteratorationSteamBenchmark.class.getSimpleName())
-                    .forks(1)
-                    .build();
-    new Runner(opt).run();
-  }
+//   public static void main(String[] args) throws RunnerException {
+//     Options opt = new OptionsBuilder()
+//                     .include(IteratorationSteamBenchmark.class.getSimpleName())
+//                     .forks(1)
+//                     .build();
+//     new Runner(opt).run();
+//   }
 
-}
+// }

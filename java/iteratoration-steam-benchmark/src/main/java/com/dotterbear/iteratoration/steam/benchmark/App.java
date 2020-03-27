940f2676c93459863772ffc1a58d9e6a4266e33f
Kan <safghjkl@gmail.com>
2020-03-27 14:54:50 +0900

    update main method

--- java/iteratoration-steam-benchmark/src/main/java/com/dotterbear/iteratoration/steam/benchmark/App.java
+++ java/iteratoration-steam-benchmark/src/main/java/com/dotterbear/iteratoration/steam/benchmark/App.java
@@ -1,4 +1,13 @@
 package com.dotterbear.iteratoration.steam.benchmark;
 
 public class App {
+
+  public static void main(String[] args) throws RunnerException {
+    Options opt = new OptionsBuilder()
+                    .include(IteratorationSteamBenchmark.class.getSimpleName())
+                    .forks(1)
+                    .build();
+    new Runner(opt).run();
+  }
+
 }

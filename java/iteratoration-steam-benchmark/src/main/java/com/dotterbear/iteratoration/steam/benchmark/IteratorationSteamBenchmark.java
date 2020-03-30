1f391cd3ee9f24373056824629a591180c778c96
Kan <safghjkl@gmail.com>
2020-03-30 13:35:48 +0900

    Update IteratorationSteamBenchmark.java

--- java/iteratoration-steam-benchmark/src/main/java/com/dotterbear/iteratoration/steam/benchmark/IteratorationSteamBenchmark.java
+++ java/iteratoration-steam-benchmark/src/main/java/com/dotterbear/iteratoration/steam/benchmark/IteratorationSteamBenchmark.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.LinkedList;
 import java.util.Iterator;
 import java.util.ListIterator;
 import java.util.concurrent.TimeUnit;

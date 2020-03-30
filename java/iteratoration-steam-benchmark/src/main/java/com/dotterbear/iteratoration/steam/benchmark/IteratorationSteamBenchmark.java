59ada3edd0344354078212eb137f1c35f1f3b6cf
Kan <safghjkl@gmail.com>
2020-03-30 15:34:38 +0900

    remove state

--- java/iteratoration-steam-benchmark/src/main/java/com/dotterbear/iteratoration/steam/benchmark/IteratorationSteamBenchmark.java
+++ java/iteratoration-steam-benchmark/src/main/java/com/dotterbear/iteratoration/steam/benchmark/IteratorationSteamBenchmark.java
@@ -16,7 +16,7 @@ import java.util.stream.Collectors;
 @Fork(1)
 @Warmup(iterations = 2, time = 500, timeUnit = TimeUnit.MILLISECONDS)
 @Measurement(iterations = 2, time = 500, timeUnit = TimeUnit.MILLISECONDS)
-@State(Scope.Thread)
+// @State(Scope.Thread)
 public class IteratorationSteamBenchmark {
 
   public static final int N = 500;
